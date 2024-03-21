408bc9cf135177f6e080e060ebd0992b9974cf30
VidipKhattar <vidipkhattar13@gmail.com>
2024-03-21 19:15:27 +0530

    Full Frontend development done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo1.svg";
 import "./App.css";
 
 import axios from "axios";
@@ -19,28 +19,65 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="bg-gradient-to-bl from-blue-400 to-orange-500 via-purple-500 animate-gradient-x min-h-screen flex justify-center items-center">
+      <div className="bg-gradient-to-bl from-blue-400 to-green-500 via-orange-500 animate-gradient-x min-h-screen lg:flex lg:justify-center lg:items-center  md:flex md:justify-center md:items-center">
         <div className="container mx-auto px-4 text-center">
-          <header className="text-2xl font-bold mb-4 text-white">
-            Guess the Sample!
+          <header className="text-6xl font-bold mb-4 text-white">
+            samplele.
           </header>
-          <h1 className="text-xl font-semibold mb-2 text-white">Rap</h1>
-          <div className="grid grid-cols-1 gap-4">
-            {this.state.details.map((output, id) => (
-              <div
-                key={id}
-                className="bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-4 rounded-md shadow-lg"
+          <div className="grid grid-cols-1 md:grid-cols-10 sm:grid-cols-1 gap-4">
+            <input
+              type="text"
+              className="lg:col-span-6 md:col-span-6 sm:col-span-4 bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-2 rounded-xl shadow-lg md:mb-10"
+              placeholder="Search Song.."
+            />
+            <div className="md:col-span-4 sm:col-span-4 grid grid-cols-2 gap-4">
+              <button
+                className="bg-blue-600 hover:bg-opacity-80 transition-colors duration-300 ease-in-out bg-opacity-50 backdrop-filter backdrop-blur-lg p-2 rounded-xl shadow-lg mb-10 w-full font-bold text-white"
+                type="button"
               >
-                <h2 className="text-lg font-semibold">
-                  {output.sampler_title}
-                </h2>
-                <p className="text-gray-600">{output.sampler_artist}</p>
-                <p className="text-gray-600">{output.sampler_year}</p>
-
-                {/* Add more details here */}
-              </div>
-            ))}
+                Lock In
+              </button>
+              <button
+                className="flex items-center justify-center bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-2 rounded-xl shadow-lg mb-10"
+                type="button"
+              >
+                <img src={logo} alt="Logo" />
+              </button>
+            </div>
           </div>
+
+          {this.state.details.map((output, id) => (
+            <div
+              key={id}
+              className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4"
+            >
+              <div className="md:col-span-2 sm:col-span-1 bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-4 rounded-3xl shadow-lg grid grid-cols-1 gap-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 flex flex-col md:flex-row justify-center items-center">
+                <div className="flex justify-center items-center md:block sm:block">
+                  <div className="bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-4 rounded-3xl shadow-lg w-48 h-48 flex justify-center items-center">
+                    <button
+                      className="bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-2 rounded-full shadow-lg "
+                      type="button"
+                    >
+                      <img src={logo} alt="Logo" className="w-12 h-12" />
+                    </button>
+                  </div>
+                </div>
+
+                <div className="col-span-1 text-center md:text-left">
+                  <p className="font-bold text-2xl py-4">
+                    {output.sampler_title}
+                  </p>
+                  <p className="text-xl pb-4">{output.sampler_title}</p>
+                  <p className="text-xl pb-4">{output.sampler_artist}</p>
+                  <p className="text-xl pb-4">{output.sampler_year}</p>
+                </div>
+              </div>
+
+              <div className="lg:col-span-1 sm:col-span-2 bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-4 rounded-3xl shadow-lg">
+                <p className="py-2">{output.clue_2}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     );
