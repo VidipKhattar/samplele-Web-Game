c1577edecd295d16270bef42f121df998a4f1c50
VidipKhattar <vidipkhattar13@gmail.com>
2024-04-14 09:20:35 +0530

    Added new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import MainPage from "./pages/mainPage";
 import AdminLogin from "./pages/adminLogin";
+import Login from "./pages/login";
 import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="admin" element={<AdminLogin />} />
+        <Route path="admin" element={<Login />} />
+
         <Route path="easteregg" element={<div>HAHAHHAHAHAHA</div>} />
       </Routes>
     </BrowserRouter>
