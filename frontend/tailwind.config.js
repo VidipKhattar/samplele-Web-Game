867b60f03df9caf052058f5be9459f2f5e746a0f
VidipKhattar <vidipkhattar13@gmail.com>
2024-04-14 09:30:46 +0530

    added css props for animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,33 @@ module.exports = {
         "gradient-x": "gradient-x 15s ease infinite",
         "gradient-y": "gradient-y 15s ease infinite",
         "gradient-xy": "gradient-xy 15s ease infinite",
+        shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+        "flash-red": "flash-red 2s linear",
+        "flash-green": "flash-green 2s linear",
       },
       keyframes: {
+        shake: {
+          "10%, 90%": {
+            transform: "translate3d(-1px, 0, 0)",
+          },
+          "20%, 80%": {
+            transform: "translate3d(2px, 0, 0)",
+          },
+          "30%, 50%, 70%": {
+            transform: "translate3d(-4px, 0, 0)",
+          },
+          "40%, 60%": {
+            transform: "translate3d(4px, 0, 0)",
+          },
+        },
+        "flash-red": {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
+        "flash-green": {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
         "gradient-y": {
           "0%, 100%": {
             "background-size": "400% 400%",
