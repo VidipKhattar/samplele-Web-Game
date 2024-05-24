604c03fb725b59765f68065d6aa7ef5de724addb
VidipKhattar <vidipkhattar13@gmail.com>
2024-05-25 01:00:20 +0530

    added ability to repost songs

--- frontend/src/components/songTable.js
+++ frontend/src/components/songTable.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function AdminPage() {
   const [songPosts, setSongPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedDates, setSelectedDates] = useState({});
+  const [disabledDates, setDisabledDates] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [datePickerLoading, setDatePickerLoading] = useState({}); // New state for individual loading status
 
   useEffect(() => {
     axios
@@ -13,6 +19,12 @@ function AdminPage() {
           : `${process.env.REACT_APP_API_BASE_URL_DEV}/songposts`
       )
       .then((response) => {
+        let disabledDates = [];
+        for (let i = 0; i < response.data.length; i++) {
+          disabledDates.push(new Date(response.data[i].post_date));
+        }
+        setDisabledDates(disabledDates);
+
         response.data.sort((a, b) => {
           let dateA = new Date(a.post_date);
           let dateB = new Date(b.post_date);
@@ -20,12 +32,30 @@ function AdminPage() {
         });
         setSongPosts(response.data);
         setLoading(false);
+
+        // Find the first available (non-disabled) date
+        const today = new Date();
+        let availableDate = today;
+        while (
+          disabledDates.some(
+            (date) => date.toDateString() === availableDate.toDateString()
+          )
+        ) {
+          availableDate.setDate(availableDate.getDate() + 1);
+        }
+        setStartDate(availableDate);
       })
       .catch((error) => {
         console.error("Error fetching song posts:", error);
         setLoading(false);
       });
   }, []);
+
+  const filterDisabledDates = (date) => {
+    return !disabledDates.some(
+      (disabledDate) => disabledDate.toDateString() === date.toDateString()
+    );
+  };
 
   const handleSongDelete = (id) => {
     const updatedSongPosts = songPosts.map((post) => {
@@ -52,6 +82,80 @@ function AdminPage() {
       });
   };
 
+  const handleDateChange = (id, date) => {
+    setDatePickerLoading((prevLoading) => ({
+      ...prevLoading,
+      [id]: true,
+    }));
+
+    setSelectedDates((prevDates) => ({
+      ...prevDates,
+      [id]: date,
+    }));
+
+    const processUrl = (url) => {
+      const start = "https://samplele-bucket.s3.amazonaws.com/songs/";
+      if (url.startsWith(start)) {
+        const strippedUrl = url.substring(start.length).split("?")[0];
+        return decodeURIComponent(strippedUrl);
+      }
+      return url;
+    };
+
+    const baseUrl =
+      process.env.NODE_ENV === "production"
+        ? process.env.REACT_APP_API_BASE_URL_PROD
+        : process.env.REACT_APP_API_BASE_URL_DEV;
+
+    axios
+      .get(`${baseUrl}/songposts/${id}`)
+      .then((response) => {
+        const { sampled_audio, sampler_audio, post_date } = response.data;
+        console.log(post_date);
+        console.log(date);
+        const localDateString = new Date(
+          date.getTime() - date.getTimezoneOffset() * 60000
+        )
+          .toISOString()
+          .split("T")[0];
+        console.log(localDateString);
+        response.data.sampled_audio = processUrl(sampled_audio);
+        response.data.sampler_audio = processUrl(sampler_audio);
+        response.data.post_date = localDateString;
+        console.log(response.data);
+        repostSong(response.data, id);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error fetching song post:", error);
+        setDatePickerLoading((prevLoading) => ({
+          ...prevLoading,
+          [id]: false,
+        }));
+      });
+  };
+
+  const repostSong = async (songPostData, id) => {
+    try {
+      await axios.post(
+        process.env.NODE_ENV === "production"
+          ? `${process.env.REACT_APP_API_BASE_URL_PROD}/songposts/`
+          : `${process.env.REACT_APP_API_BASE_URL_DEV}/songposts/`,
+        songPostData
+      );
+      setDatePickerLoading((prevLoading) => ({
+        ...prevLoading,
+        [id]: false,
+      }));
+    } catch (error) {
+      alert(error);
+      setDatePickerLoading((prevLoading) => ({
+        ...prevLoading,
+        [id]: false,
+      }));
+    }
+  };
+
   const renderSongPosts = () => {
     const today = new Date();
     const todayDateString = today.toISOString().split("T")[0];
@@ -63,6 +167,37 @@ function AdminPage() {
         }`}
       >
         <td className="py-4 px-6">{post.post_date}</td>
+        <td className="py-4 px-6">
+          {datePickerLoading[post.id] ? (
+            <div className="flex justify-center items-center">
+              <div
+                className="inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                role="status"
+              ></div>
+            </div>
+          ) : (
+            <DatePicker
+              selected={selectedDates[post.id] || null}
+              onChange={(date) => handleDateChange(post.id, date)}
+              minDate={new Date()}
+              openToDate={startDate} // Set the initial month
+              filterDate={filterDisabledDates}
+              dateFormat="yyyy-MM-dd"
+              placeholderText="Select a date"
+              className="grid-cols-2 w-full bg-white bg-opacity-25 backdrop-filter backdrop-blur-lg p-2 rounded-xl shadow-lg mb-1"
+              popperProps={{
+                modifiers: [
+                  {
+                    name: "offset",
+                    options: {
+                      offset: [0, 10],
+                    },
+                  },
+                ],
+              }}
+            />
+          )}
+        </td>
         <td className="py-4 px-6">
           {post.sampler_title + " - " + post.sampler_artist}
         </td>
@@ -98,6 +233,7 @@ function AdminPage() {
           <thead>
             <tr>
               <th className="py-4 px-6">Post Date</th>
+              <th className="py-4 px-6">Repost</th>
               <th className="py-4 px-6">Sampler Song</th>
               <th className="py-4 px-6">Original Song</th>
               <th className="py-4 px-6">Delete</th>
@@ -106,7 +242,7 @@ function AdminPage() {
           <tbody>
             {loading ? (
               <tr>
-                <td colSpan="4" className="py-8">
+                <td colSpan="5" className="py-8">
                   <div className="flex justify-center items-center">
                     <div
                       className="inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
